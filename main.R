3558e0a0cf761ee077fac4b1642af19d4db80149
Jahna A Thompson <jahna.thompson@wsu.edu>
2026-03-30 19:37:00 -0700

    shorten names

--- main.R
+++ main.R
@@ -7,9 +7,9 @@ library(here)
 library(dplyr)
 
 #load dataset and create pipe -- %>%
-tv_hours_table <-gss_cat %>%
+tv_hours_tab <-gss_cat %>%
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours=mean(tvhours,na.rm=TRUE))
 
-write.csv(tv_hours_table,here("TV_HOURS_BY_MARITAL.csv"))
+write.csv(tv_hours_tab,here("TV_HOURS_BY_MARITAL.csv"))
